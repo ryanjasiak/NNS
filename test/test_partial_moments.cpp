e0beff73b5e497e4e8216607816e28ef0aa29c78
Erik Ketterer <ekk5328@psu.edu>
2025-07-20 16:26:46 -0500

    Added Version 2 test cases to test_partial_moments

--- test/test_partial_moments.cpp
+++ test/test_partial_moments.cpp
@@ -104,3 +104,102 @@ TEST_CASE("NNS::divergent_partial_moment - 1")
     Rcpp::Rcout << "Lower CPM C++ = " << cpm_lo_cpp << "\n";
     Rcpp::Rcout << "Lower CPM R   = " << cpm_lo_r << "\n";
 }
+
+TEST_CASE("Version 2: NNS::partial_moment - 1")
+{
+    // Get the R inside singleton
+    RInside &R = NNS_test::rinside::r();
+
+    //-------------------------------------------------
+    // partial_moment
+    //-------------------------------------------------
+
+    // Create sample data
+    std::vector<double> v = {5.0, 2.5, 2.2, 3.8};
+    Eigen::VectorXd e_v = Eigen::Map<Eigen::VectorXd>(v.data(), v.size());
+    Rcpp::NumericVector r_v(v.begin(), v.end());
+    R["v"] = r_v;
+
+    double degree = 3.0; // e.g. third-order moment
+    double target = 6.0; // threshold/benchmark
+    R["d"] = degree;
+    R["t"] = target;
+
+    double e_res = NNS::partial_moment<NNS::Tail::Lower>(degree, target, e_v);
+    double r_res = R.parseEval("LPM(d, t, v)");
+    Rcpp::Rcout << "e_res: " << e_res << "\n";
+    Rcpp::Rcout << "r_res: " << r_res << "\n";
+    REQUIRE(e_res == Catch::Approx(r_res).margin(1e-12));
+}
+
+TEST_CASE("Version 2: NNS::co_partial_moment - 1")
+{
+    // R instance --------------------------------------------------------------
+    RInside &R = NNS_test::rinside::r();
+
+    // sample data -------------------------------------------------------------
+    Eigen::VectorXd x(5);
+    x << 0.05, 0.02, 0.03, -0.02, 0.07;
+    Eigen::VectorXd y(5);
+    y << 0.03, 0.01, 0.02, -0.04, 0.05;
+
+    double k  = 3.0;   // moment degree
+    double Tx = 1.0;   // targets
+    double Ty = 1.0;
+
+    // shove C++ data into R ----------------------------------------------------
+    R["x"]  = Rcpp::NumericVector(x.data(), x.data() + x.size());
+    R["y"]  = Rcpp::NumericVector(y.data(), y.data() + y.size());
+    R["k"]  = k;
+    R["Tx"] = Tx;
+    R["Ty"] = Ty;
+
+    // ---- upper–tail CPM ------------------------------------------------------
+    double cpm_up_cpp = NNS::co_partial_moment<NNS::Tail::Upper>(k, x, y, Tx, Ty);
+    double cpm_up_r   = R.parseEval("Co.UPM(k, x, y, Tx, Ty)");
+    REQUIRE(cpm_up_cpp == Catch::Approx(cpm_up_r).margin(1e-12));
+    Rcpp::Rcout << "Lower CPM C++ = " << cpm_up_cpp << "\n";
+    Rcpp::Rcout << "Lower CPM R   = " << cpm_up_r << "\n";
+
+    double cpm_lo_cpp = NNS::co_partial_moment<NNS::Tail::Lower>(k, x, y, Tx, Ty);
+    double cpm_lo_r   = R.parseEval("Co.LPM(k, x, y, Tx, Ty)");
+    REQUIRE(cpm_lo_cpp == Catch::Approx(cpm_lo_r).margin(1e-12));
+    Rcpp::Rcout << "Lower CPM C++ = " << cpm_lo_cpp << "\n";
+    Rcpp::Rcout << "Lower CPM R   = " << cpm_lo_r << "\n";
+}
+
+TEST_CASE("Version 2: NNS::divergent_partial_moment - 1")
+{
+    // R instance --------------------------------------------------------------
+    RInside &R = NNS_test::rinside::r();
+
+    // sample data -------------------------------------------------------------
+    Eigen::VectorXd x(5);
+    x << 0.02, 0.07, 0.04, -0.03, 0.01;
+    Eigen::VectorXd y(5);
+    y << 0.02, 0.04, 0.03, -0.04, 0.05;
+
+    double k  = 3.0;   // moment degree
+    double Tx = 0.0;   // targets
+    double Ty = 0.0;
+
+    // shove C++ data into R ----------------------------------------------------
+    R["x"]  = Rcpp::NumericVector(x.data(), x.data() + x.size());
+    R["y"]  = Rcpp::NumericVector(y.data(), y.data() + y.size());
+    R["k"]  = k;
+    R["Tx"] = Tx;
+    R["Ty"] = Ty;
+
+    // ---- upper–tail CPM ------------------------------------------------------
+    double cpm_up_cpp = NNS::divergent_partial_moment<NNS::Tail::Upper, NNS::Tail::Lower>(k, x, y, Tx, Ty);
+    double cpm_up_r   = R.parseEval("D.UPM(k, k, x, y, Tx, Ty)");
+    REQUIRE(cpm_up_cpp == Catch::Approx(cpm_up_r).margin(1e-12));
+    Rcpp::Rcout << "Lower CPM C++ = " << cpm_up_cpp << "\n";
+    Rcpp::Rcout << "Lower CPM R   = " << cpm_up_r << "\n";
+
+    double cpm_lo_cpp = NNS::divergent_partial_moment<NNS::Tail::Lower, NNS::Tail::Upper>(k, x, y, Tx, Ty);
+    double cpm_lo_r   = R.parseEval("D.LPM(k, k, x, y, Tx, Ty)");
+    REQUIRE(cpm_lo_cpp == Catch::Approx(cpm_lo_r).margin(1e-12));
+    Rcpp::Rcout << "Lower CPM C++ = " << cpm_lo_cpp << "\n";
+    Rcpp::Rcout << "Lower CPM R   = " << cpm_lo_r << "\n";
+}
